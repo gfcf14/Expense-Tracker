530abd285819b550ba5bb9479bede07097c0def2
Carlos <carloscuba014@gmail.com>
2024-05-30 11:42:12 -0400

    create model for category

--- Expense Tracker/obj/Debug/net8.0/Expense Tracker.AssemblyInfo.cs
+++ Expense Tracker/obj/Debug/net8.0/Expense Tracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fa85fec722227effd97d03908add9ade3fa89b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
